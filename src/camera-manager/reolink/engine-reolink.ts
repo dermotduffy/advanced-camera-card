b6c63f32e3ae33f99257bf6d977f556d08feb6d5
dermotduffy <dermot.duffy@gmail.com>
2025-05-27 14:39:42 +0000

    Remove duplicate sort code

--- src/camera-manager/reolink/engine-reolink.ts
+++ src/camera-manager/reolink/engine-reolink.ts
@@ -2,7 +2,6 @@ import { add, endOfDay, parse, startOfDay } from 'date-fns';
 import { orderBy } from 'lodash-es';
 import { CameraConfig } from '../../config/schema/cameras';
 import { getViewMediaFromBrowseMediaArray } from '../../ha/browse-media/browse-media-to-view-media';
-import { sortMediaByStartDate } from '../../ha/browse-media/sort-browse-media-by-start-date';
 import {
   BROWSE_MEDIA_CACHE_SECONDS,
   BrowseMedia,
@@ -213,8 +212,6 @@ export class ReolinkCameraManagerEngine extends BrowseMediaCameraManagerEngine {
           matcher: (media: RichBrowseMedia<BrowseMediaMetadata>) =>
             media.can_expand &&
             isMediaWithinDates(media, matchOptions?.start, matchOptions?.end),
-          sorter: (media: RichBrowseMedia<BrowseMediaMetadata>[]) =>
-            sortMediaByStartDate(media),
         },
       ],
       {
@@ -278,8 +275,6 @@ export class ReolinkCameraManagerEngine extends BrowseMediaCameraManagerEngine {
               matcher: (media: RichBrowseMedia<BrowseMediaMetadata>) =>
                 !media.can_expand &&
                 isMediaWithinDates(media, perCameraQuery.start, perCameraQuery.end),
-              sorter: (media: RichBrowseMedia<BrowseMediaMetadata>[]) =>
-                sortMediaByStartDate(media),
             },
           ],
           {
--- src/ha/browse-media/sort-browse-media-by-start-date.ts
+++
@@ -1,8 +0,0 @@
-import { orderBy } from 'lodash-es';
-import { BrowseMediaMetadata, RichBrowseMedia } from './types';
-
-export const sortMediaByStartDate = (
-  media: RichBrowseMedia<BrowseMediaMetadata>[],
-): RichBrowseMedia<BrowseMediaMetadata>[] => {
-  return orderBy(media, (media) => media._metadata?.startDate, 'desc');
-};
